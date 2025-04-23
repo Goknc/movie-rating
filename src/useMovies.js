476a7c502cb6b17edb952d51aa975d0a19a57966
Gökhan Çavdar <82711284+Goknc@users.noreply.github.com>
2025-04-24 00:05:21 +0300

    Update useMovies.js

--- src/useMovies.js
+++ src/useMovies.js
@@ -13,7 +13,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=3b828676&s=${query}`,
+            `https://www.omdbapi.com/?i=tt3896198&apikey=3b828676&s=${query}`,
             { signal: controller.signal }
           );
 
